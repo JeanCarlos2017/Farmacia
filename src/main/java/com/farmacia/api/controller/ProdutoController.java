cf6e89775be67900f98984868d2596c4fe324ea8
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-02-23 18:22:39 -0300

    HttpStatus para o delete

--- src/main/java/com/farmacia/api/controller/ProdutoController.java
+++ src/main/java/com/farmacia/api/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.farmacia.domain.model.ProdutoEntidade;
@@ -54,6 +56,7 @@ public class ProdutoController {
 	}
 	
 	@DeleteMapping("/{id_produto}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable long id_produto) {
 		produtoService.deleteProduto(id_produto);
 	}
